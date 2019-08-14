bf7abc277783e0feef44ccc498f5c0304132f144
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-08-14 09:45:31 +0800

    Update and rename README.md to login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+  <title>Log in</title>
+  </head>
+  <body>
+    <h1 align="Center">Rivas Images</h1>
+    <main>
+      <h2>Log in</h2>
+      <form>
+      <div>
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Password" name="psw" required>
+        
+        <button type="submit">Log in</button>
+      </div>
+      
+      <div>
+      <button type="button" class="cancelbtn">Cancel</button>
+      <span class="psw"><a href="forgotpassword.php">Forgot Password?</a></span>
+      </div>
+      </form>
+      
+      </body>
+      </html>
